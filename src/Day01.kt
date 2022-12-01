fef30ab9ba288675b2d1d43950224dc1502450e7
Dan Nguyen <dnguyen@justworks.com>
2022-12-01 16:17:47 -0500

    add example from description

--- src/Day01.kt
+++ src/Day01.kt
@@ -18,7 +18,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
