8dbf5c31e854b5f251072d4a2c97f00ad92c73e5
Dan Nguyen <dnguyen@justworks.com>
2022-12-02 11:14:55 -0500

    Day2 part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -4,6 +4,11 @@ fun main() {
         2 to 1,
         3 to 2
     )
+
+    val losesMap = beatsMap
+        .map { (k,v) -> v to k }
+        .toMap()
+
     val pointValues = mapOf(
         "A" to 1,
         "B" to 2,
@@ -13,21 +18,44 @@ fun main() {
         "Z" to 3
     )
 
-    fun solution(input: List<String>): Int {
+    val resultMap = mapOf(
+        "X" to 0,
+        "Y" to 3,
+        "Z" to 6
+    )
+
+    fun part1(input: List<String>): Int {
         return input
             .map { it.split(" ") }
             .map { listOf(pointValues[it[0]]!!, pointValues[it[1]]!!) }
             .sumOf { (elfShape, myShape) ->
-                myShape + (if (myShape==elfShape) 3
-                else if (beatsMap[myShape]==elfShape) 6
-                else 0)
+                myShape + when (elfShape) {
+                    myShape -> 3
+                    beatsMap[myShape]!! -> 6
+                    else -> 0
+                }
+            }
+    }
+
+    fun part2(input: List<String>): Int {
+        return input
+            .map { it.split(" ") }
+            .map { listOf(pointValues[it[0]]!!, resultMap[it[1]]!!) }
+            .sumOf { (elfShape, result) ->
+                result + when (result) {
+                    3 -> elfShape
+                    0 -> beatsMap[elfShape]!!
+                    else -> losesMap[elfShape]!!
+                }
             }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(solution(testInput)==15)
+    check(part1(testInput)==15)
+    check(part2(testInput)==12)
 
     val input = readInput("Day02")
-    println(solution(input))
+    println(part1(input))
+    println(part2(input))
 }
