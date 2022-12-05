df005011d604989f3ee09700d5dc3fa9c1ed85cb
Dan Nguyen <dnguyen@justworks.com>
2022-12-05 11:39:30 -0500

    Day 3 part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,32 @@
 fun main() {
     fun part1(input: List<String>): Int {
         return input
-            .flatMap { sack ->
-                val priorities = sack.map { if (it in 'a'..'z') it - 0x60 else it - 0x26 }.map { it.code }
-                val first = priorities.subList(0, priorities.size / 2)
-                val second = priorities.subList(priorities.size / 2, priorities.size)
-                first.intersect(second.toSet()).toList()
+            .flatMap {
+                val first = it.subSequence(0, it.length / 2).toSet()
+                val second = it.subSequence(it.length / 2, it.length).toSet()
+                first.intersect(second).toList()
             }
-            .sum()
+            .map { if (it in 'a'..'z') it - 0x60 else it - 0x26 }
+            .sumOf { it.code }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(3, 3)
+            .flatMap { (first, second, third) ->
+                first.toSet()
+                    .intersect(second.toSet())
+                    .intersect(third.toSet())
+                    .toList()
+            }
+            .map { if (it in 'a'..'z') it - 0x60 else it - 0x26 }
+            .sumOf { it.code }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day03_test")
     check(part1(testInput)==157)
+    check(part2(testInput)==70)
+
 
     val input = readInput("Day03")
     println(part1(input))
